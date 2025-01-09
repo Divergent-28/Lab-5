57a38b186e977918732d7e6426c235d27b548aee
Divergent-28 <logichno28@gmail.com>
2025-01-09 17:56:46 +0400

    Add files via upload

---
+++ ArrSort.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class ArrSort
+    {
+        private int[] arr;
+        private int[] arr1;
+        private int len;
+        public ArrSort()
+        {
+            this.len = 10;
+            this.arr = new int[this.len];
+            this.arr1 = new int[this.len];
+        }
+        public ArrSort(int len)
+        {
+            this.len = len;
+            this.arr = new int[this.len];
+        }
+        public int len1
+        {
+            get { return this.len; }
+        }
+        public int[] array
+        {
+            get { return this.arr; }
+        }
+
+        public int[] RandomArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < this.arr.Length; i++)
+            {
+                this.arr[i] = rnd.Next(-100, 100);
+            }
+            return this.arr;
+        }
+        public void OutputArray()
+        {
+            foreach (int i in this.arr)
+                Console.WriteLine("{0}   ", i);
+            Console.WriteLine("");
+        }
+        public void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+        public int[] BubbleSort()
+        {
+            var length = this.arr.Length;
+            for (var i = 1; i < length; i++)
+            {
+                for (var j = 0; j < length - i; j++)
+                {
+                    if (this.arr[j] > this.arr[j + 1])
+                    {
+                        Swap(ref this.arr[j], ref this.arr[j + 1]);
+                    }
+                }
+            }
+            return this.arr;
+        }
+        public int[] ShakerSort()
+        {
+            for (var i = 0; i < this.arr1.Length / 2; i++)
+            {
+                var swapFlag = false;
+                for (var j = i; j < this.arr1.Length - i - 1; j++)
+                {
+                    if (this.arr1[j] > this.arr1[j + 1])
+                    {
+                        Swap(ref this.arr1[j], ref this.arr1[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+                for (var j = this.arr1.Length - 2 - i; j > i; j--)
+                {
+                    if (this.arr1[j - 1] > this.arr1[j])
+                    {
+                        Swap(ref this.arr1[j - 1], ref this.arr1[j]);
+                        swapFlag = true;
+                    }
+                }
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+
+            return this.arr1;
+        }
+        public int[] ArrClone()
+        {
+            int[] arr1 = new int[this.len];
+            for (int i = 0; i < this.len; i++)
+            {
+                arr1[i] = this.arr[i];
+            }
+            return arr1;
+        }
+        public void Sortings()
+        {
+            int n;
+            long res1;
+            long res2;
+            int[] arr = new int[this.len];
+            int[] arr1 = new int[this.len];
+            Stopwatch timer = new Stopwatch();
+            this.arr=RandomArray();
+            OutputArray();
+            this.arr1 = ArrClone();
+            timer.Start();
+            BubbleSort();
+            timer.Stop();
+            res1 = timer.ElapsedTicks;
+            OutputArray();
+            Console.WriteLine("Время выполнения сортировки: " + res1);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            timer.Reset();
+            timer.Start();
+            ShakerSort();
+            timer.Stop();
+            res2 = timer.ElapsedTicks;
+            OutputArray();
+            Console.WriteLine("Время выполнения сортировки: " + res2);
+            if (res1 < res2)
+            {
+                Console.WriteLine("Сортировка пузырьком быстрее");
+            }
+            else if (res1 == res2)
+            {
+                Console.WriteLine("Сортировки выполняются за одно и то же время");
+            }
+            else
+            {
+                Console.WriteLine("Сортировка перемешиванием быстрее");
+            }
+            RoAVCheck.Continue();
+        }
+    }
+}
---
+++ RoAVCheck.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal static class RoAVCheck
+    {
+        public static void Continue()
+        {
+            Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+            Console.ReadLine();
+        }
+        public static int Input()
+        {
+            int c;
+            while (!int.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine($"Ошибка! введите число заново: ");
+            }
+            return c;
+        }
+        public static int InputA()
+        {
+            int A = 0;
+            bool isNumber = false;
+            while (isNumber == false)
+            {
+                Console.Write("Введите А: ");
+                string a = Console.ReadLine();
+                isNumber = int.TryParse(a, out A);
+                if (isNumber == false)
+                {
+                    Console.WriteLine("Введите A: ");
+                    while (!int.TryParse(Console.ReadLine(), out A))
+                    {
+                        Console.WriteLine("Введите A: ");
+                    }
+                }
+                if (Math.Cos(A) == 1)
+                {
+                    Console.WriteLine("Ошибка! Деление на ноль!");
+                    Continue();
+                    break;
+                }
+            }
+            return A;
+        }
+        public static int InputB()
+        {
+            int B = 0;
+            bool isNumber2 = false;
+            while (isNumber2 == false)
+            {
+                Console.Write("Введите B: ");
+                string b = Console.ReadLine();
+                isNumber2 = int.TryParse(b, out B);
+                if (isNumber2 == false)
+                {
+                    Console.WriteLine("Введите B: ");
+                    while (!int.TryParse(Console.ReadLine(), out B))
+                    {
+                        Console.WriteLine("Введите B: ");
+                    }
+                }
+                if (B <= 0)
+                {
+                    Console.WriteLine("Ошибка! Действие не может выполняться за пределами области допустимых значенимй");
+                    Continue();
+                    break;
+                }
+            }
+            return B;
+        }
+    }
+}
